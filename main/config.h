f0d28961c88d584ce2a96eb77349fc12bb3e3320
der-johanni <ivangorbanwork@gmail.com>
2025-08-21 20:45:14 +0300

    config updated

--- main/config.h
+++ main/config.h
@@ -1,9 +1,12 @@
 #pragma once
 
-#define PUMP_PIN 4
+#define PUMP_PIN                    4
+#define SOIL_MOISTURE_SENSOR_PIN    34
 
-#define SLEEP_TIME_MS 10000L
+// 10 minutes
+#define SLEEP_TIME_MS 600000L
+
+// 10 seconds
+#define DEFAULT_PUMP_TIME_MS 10000L
 
 #define MIN_SOIL_MOISTURE 50
-
-#define DEFAULT_PUMP_TIME_MS 3000L
